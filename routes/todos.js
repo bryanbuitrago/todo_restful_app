ce14c949739889279dde8aa2d813690e37d1d47f
bryan <bryanbuitrago86@gmail.com>
2019-04-03 01:08:13 -0400

    Add find and update todo route

--- routes/todos.js
+++ routes/todos.js
@@ -23,4 +23,10 @@ router.get('/:todoId', (req, res) => {
   .then((foundTodo) => res.status(201).json(foundTodo))
   .catch((err) => res.send(err));
 });
+// Update a todo
+router.put('/:todoId', (req, res) =>{
+  db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+  .then((todo) => res.json(todo))
+  .catch((err) => res.send(err));
+});
 module.exports = router;
