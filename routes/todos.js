0e025059e5344207b27eb10e122b4969a86d8cbf
bryan <bryanbuitrago86@gmail.com>
2019-04-03 01:13:40 -0400

    Rearrange comment lines

--- routes/todos.js
+++ routes/todos.js
@@ -4,25 +4,27 @@ let db = require('../models');
 
 
 
+// Get all todos
 router.get('/', (req, res) => {
-  // Get all TODOS from mongoDB
   db.Todo.find()
   .then( (todos) => res.json(todos))
   .catch((err) => res.send(err))
 });
 
+// Create a todo
 router.post('/', (req, res) => {
-  // Create a todo
   db.Todo.create(req.body)
   .then((newTodo) => res.status(201).json(newTodo))
   .catch((err) => res.send(err));
 });
+
 // Find a todo
 router.get('/:todoId', (req, res) => {
   db.Todo.findById(req.params.todoId)
   .then((foundTodo) => res.status(201).json(foundTodo))
   .catch((err) => res.send(err));
 });
+
 // Update a todo
 router.put('/:todoId', (req, res) =>{
   db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
