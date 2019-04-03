45c011b0bd84c04e2cd6bf8258defe11dbc75cbb
bryan <bryanbuitrago86@gmail.com>
2019-04-03 00:44:06 -0400

    Add body-parser

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ let express = require('express'),
     app = express(),
     port = process.env.PORT || 8080,
     log = console.log,
+    bodyParser = require('body-parser'),
     todoRoutes = require('./routes/todos');
+
+// body-parser setup
+// allows access to request body from put or post request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('HELLO FROM THE ROOT ROUTE');
