0ac7ca3a0a663e974bd9c0fbd0c0a4f3b8b17b70
bryan <bryanbuitrago86@gmail.com>
2019-04-02 20:37:39 -0400

    Mongoose Setup

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// MONGOOSE SETUP
+let mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost/todo-api');
+
+mongoose.promise = Promise;
+
+module.exports.Todo = require('./todo');
