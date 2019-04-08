8a33fe283edbf6b81e6764395d71e2f893c28a63
bryan <bryanbuitrago86@gmail.com>
2019-04-08 02:48:49 -0400

    Add post todo logic.

--- public/app.js
+++ public/app.js
@@ -1,13 +1,39 @@
 $(document).ready(function(){
   $.getJSON('api/todos')
   .then(addTodos)
+
+  $('#todoInput').keypress(function(event) {
+    if(event.which === 13) {
+      createTodo();
+    }
+  })
 });
 
 function addTodos(todos) {
   // add todos to the page
   todos.forEach(function(todo){
-    var newTodo = $('<li>' + todo.name + ' </li>')
-    newTodo.addClass('task');
-    $('.list').append(newTodo);
+    addTodo(todo);
+  });
+}
+
+function addTodo(todo) {
+  var newTodo = $('<li>' + todo.name + ' </li>')
+  newTodo.addClass('task');
+  if(todo.completed) {
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
+}
+
+function createTodo() {
+  // send request to create new todo
+  var userInput = $('#todoInput').val();
+  $.post('/api/todos', {name: userInput})
+  .then(function(newTodo) {
+    $('#todoInput').val('');
+    addTodo(newTodo);
+  })
+  .catch(function(err) {
+    console.log(err);
   })
 }
