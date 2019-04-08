f36ebe35f7ced23d6c0023d5971799b74304d4e0
bryan <bryanbuitrago86@gmail.com>
2019-04-08 01:28:32 -0400

    Get all todos from api

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
-alert('HELLO FROM APP.JS');
+$(document).ready(function(){
+  $.getJSON('api/todos')
+  .then(addTodos)
+});
+
+function addTodos(todos) {
+  // add todos to the page
+  todos.forEach(function(todo){
+    var newTodo = $('<li>' + todo.name + ' </li>')
+    newTodo.addClass('task');
+    $('.list').append(newTodo);
+  })
+}
